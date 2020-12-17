68ca8cc24f440dece6dd6c36c657f478a720ffa4
superpangE <cdpkct@gmail.com>
2020-12-17 16:57:08 +0900

    [style] Calendar style 적용

--- fe/src/components/MainContainer/Main/Calendar/index.jsx
+++ fe/src/components/MainContainer/Main/Calendar/index.jsx
@@ -3,14 +3,14 @@ import Item from "./Item";
 import { Container } from "./style";
 
 const Calendar = () => {
-  return (
-    <Container>
-      <Item id="beakjoon" />
-      <Item id="codeforces" />
-      <Item id="atcoder" />
-      <Item id="another" />
-    </Container>
-  );
+    return (
+        <Container>
+            <Item id="beakjoon" />
+            <Item id="codeforces" />
+            <Item id="atcoder" />
+            <Item id="another" />
+        </Container>
+    );
 };
 
 export default Calendar;
