16469318d1a22d0b90d75ddc674f565e43f137ca
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-17 15:12:06 +0900

    [feat] Header 컴포넌트 생성
    모든 페이지 상단 부분에 위치하는 Header 컴포넌트를 생성함

---
+++ fe/src/components/Header/style.js
@@ -0,0 +1,10 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  border-bottom: 1px solid #efefef;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+  color: black;
+  font-size: 25px;
+`;
