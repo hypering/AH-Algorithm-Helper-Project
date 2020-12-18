43d30ba957350823b2a8d7c1c4cb870b568ccae3
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-18 17:21:17 +0900

    [feat] 최상위에서 관리할 수 있도록 Picker의 최상위에서 useState 추가

--- fe/src/components/MainContainer/Main/Picker/index.jsx
+++ fe/src/components/MainContainer/Main/Picker/index.jsx
@@ -1,11 +1,21 @@
-import React from "react";
-import CategoryContainer from "./Category/index";
+import React, { useState } from "react";
+import CategoryContainer from "./Categories/index";
 import Difficulty from "./Difficulty/index";
 import Member from "./Member/index";
 import ProblemCount from "./ProblemCount";
 import { MainContainer, Container, SubContainer } from "./style";
 
 const Picker = () => {
+  const [problemCnt, setProblemCnt] = useState(0);
+  const [selectedCate, setSelectedCate] = useState([]);
+  const onSubmit = () => {
+    //모든 항목이 입력됐는지 확인해야함
+    if (problemCnt == 0 || problemCnt == "" || selectedCate.length === 0) {
+      alert("error");
+    } else {
+      console.log("Query gogo");
+    }
+  };
   return (
     <Container>
       <SubContainer>
@@ -13,13 +23,16 @@ const Picker = () => {
       </SubContainer>
       <SubContainer>
         <Difficulty />
-        <ProblemCount />
+        <ProblemCount problemCnt={problemCnt} setProblemCnt={setProblemCnt} />
       </SubContainer>
       <MainContainer>
-        <CategoryContainer />
+        <CategoryContainer
+          selectedCate={selectedCate}
+          setSelectedCate={setSelectedCate}
+        />
         <div>결과창</div>
       </MainContainer>
-      <div>적용</div>
+      <div onClick={onSubmit}>적용</div>
     </Container>
   );
 };
