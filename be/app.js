93037426ad709857b41e780dcc8adc44013c0c6a
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-18 11:18:39 +0900

    [fix] Backend 서버 구동시 발생하는 문제 수정
    Path 모듈이 존재하지 않는데 app.js에서 사용하고 있어서 삭제

--- be/app.js
+++ be/app.js
@@ -7,7 +7,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));S
 
 app.listen(4000);
 app.get("/getProlems/:id", (req, res) => {
