194ed084cd51cf01e6f413253156046868791001
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-18 11:26:45 +0900

    [style] 대회일정 관련 Calendar 컴포넌트 스타일 적용

--- fe/src/components/MainContainer/Main/Calendar/Item/style.js
+++ fe/src/components/MainContainer/Main/Calendar/Item/style.js
@@ -1,31 +1,28 @@
 import styled from "@emotion/styled";
 
 export const Title = styled.div`
-  font-size: 25px;
   margin: auto;
+  font-size: 25px;
 `;
-export const WhatDay = styled.div`
-  // font-size: 10px;
-`;
-export const WhatContest = styled.div`
-  // font-size: 10px;
-`;
+
 export const ContestWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-content: center;
+  padding: 15px;
+  font-size: 17px;
   &:hover {
     border: 1px groove;
-    margin: -1px 5px;
     cursor: pointer;
   }
-  margin: 0 5px;
-  padding: 5px;
-  font-size: 10px;
-  width: 200px;
 `;
+
 export const Container = styled.div`
+  display: flex;
   height: 100%;
   width: 100%;
   &:hover {
     background-color: #efefef;
   }
-  display: flex;
 `;
