fe193513d512b22fd7a41ff4e6a3920427372bb0
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-18 17:18:55 +0900

    문제 개수를 저장하는 useState 추가

--- fe/src/components/MainContainer/Main/Picker/ProblemCount/index.jsx
+++ fe/src/components/MainContainer/Main/Picker/ProblemCount/index.jsx
@@ -2,12 +2,21 @@ import React from "react";
 
 import { Subtitle, Container, Subcontainer, TextInput } from "./style";
 
-const ProblemCount = () => {
+const ProblemCount = ({ problemCnt, setProblemCnt }) => {
+  const onChange = (e) => {
+    const { value } = e.target;
+    setProblemCnt(value);
+  };
   return (
     <Container>
       <Subtitle>문제 개수</Subtitle>
       <Subcontainer>
-        <TextInput type="text" placeholder="문제 개수"></TextInput>
+        <TextInput
+          type="text"
+          placeholder="문제 개수"
+          value={problemCnt}
+          onChange={onChange}
+        ></TextInput>
       </Subcontainer>
     </Container>
   );
