9667bf861e15add979afd6e505efdd7afbe03135
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-18 17:10:29 +0900

    [feat] 웹 크롤링 기능 적용
    axios, cheerio 모듈을 사용해 Codeforces 웹에서 대회 부분만 크롤링하는 작업을 수행

--- be/app.js
+++ be/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const dotenv = require('dotenv');
 const dbStarter = require('./providers/dbProvider');
 const { contestModel, problemModel } = require('./models');
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -15,9 +17,45 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+const getHTML = async () => {
+  try {
+    return await axios.get('https://codeforces.com/contests');
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getCodeforcesContest = (contest, turm) => {
+  return {
+    name: 'codeforces',
+    title: contest[0],
+    startDate: contest[1 + turm],
+    due: contest[2 + turm],
+  };
+};
+
 app.get('/contest', async (req, res) => {
-  const contestDate = await contestModel.find();
-  res.json(contestDate);
+  const contestDatas = [];
+  const htmlDate = await getHTML();
+  const $ = cheerio.load(htmlDate.data);
+  $('div.contestList>div.datatable>div>table>tbody>tr').each((index, item) => {
+    const crollingText = $(item).find('td').text();
+    const dataContent = crollingText
+      .replace(/(\r\n\t|\n|\r\t)/gm, '')
+      .split('  ')
+      .filter((element) => element !== '');
+    if (index === 1) contestDatas.push(getCodeforcesContest(dataContent, 1));
+    else if (index !== 0) contestDatas.push(getCodeforcesContest(dataContent, 0));
+  });
+  res.json(contestDatas);
+});
+
+// 유저들의 정보 및 요구사항을 받는다.
+// DB에 있는 문제들이랑 해당 유저들이 푼 문제를 비교
+// 요구 사항에 맞는 문제들을 보내준다.
+app.get('/problem', async (req, res) => {
+  const problemDate = await problemModel.find();
+  res.json(problemDate);
 });
 
 const booting = async () => {
