35bd1888ed2c09455ee157c3fd72b3f3cc478219
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-18 17:22:06 +0900

    [feat] 적용 버튼 클릭이벤트(유효성 검사)

---
+++ fe/src/components/MainContainer/Main/Picker/Result/style.js
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const ResContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 15px;
+  width: 50%;
+`;
+
+export const Subtitle = styled.span`
+  font-weight: 600;
+  text-align: center;
+`;
