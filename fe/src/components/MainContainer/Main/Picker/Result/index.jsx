2c1f3ff54572f6c3a631de277f509097c0cf9ef1
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-21 16:58:44 +0900

    [feat][style] 결과 컴포넌트, 결과 엔티티 컴포넌트 추가 및 스타일링

--- fe/src/components/MainContainer/Main/Picker/Result/index.jsx
+++ fe/src/components/MainContainer/Main/Picker/Result/index.jsx
@@ -1,9 +1,34 @@
 import React from "react";
-import { ResContainer, Subtitle } from "./style.js";
+import ResEntity from "./ResEntity/index.jsx";
+import { ResContainer, Results, Subtitle } from "./style.js";
+import "./index.css";
 const Result = () => {
   return (
     <ResContainer>
-      <Subtitle>결과창</Subtitle>
+      <Subtitle>문제 뽑기 결과</Subtitle>
+      <Results>
+        <div className="resultHeader">
+          <span>난이도 </span>
+          <span>번호 </span>
+          <span>카테고리</span>
+        </div>
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+      </Results>
     </ResContainer>
   );
 };
