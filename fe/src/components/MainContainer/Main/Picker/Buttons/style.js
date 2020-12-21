0f242cc42527b936c0999e42b6777478db8e7225
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-21 16:51:54 +0900

    [feat]버튼모음 폴더 + Execute 버튼 컴포넌트 추가

---
+++ fe/src/components/MainContainer/Main/Picker/Buttons/style.js
@@ -0,0 +1,28 @@
+import styled from "@emotion/styled";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 50%;
+  margin-top: 15px;
+  flex-grow: 1;
+`;
+
+export const Categories = styled.div`
+  display: flex;
+  flex-direction: column;
+  overflow-y: scroll;
+`;
+
+export const Subtitle = styled.span`
+  font-weight: 600;
+`;
+
+export const Execute = styled(FontAwesomeIcon)`
+  cursor: pointer;
+  &:hover {
+    color: #385185;
+    transition: 1s;
+  }
+`;
