2d061a00094c7c675fc635fd393a0282dab680aa
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-22 16:46:05 +0900

    [feat] useState 전달로 쿼리 결과값 렌더링

--- fe/src/components/MainContainer/Main/Picker/Result/index.jsx
+++ fe/src/components/MainContainer/Main/Picker/Result/index.jsx
@@ -1,33 +1,31 @@
 import React from 'react';
 import ResEntity from './ResEntity/index.jsx';
-import { ResContainer, Results, Subtitle } from './style.js';
-import './index.css';
-const Result = () => {
+// import ResEntity from './ResEntity/index.jsx';
+import { ResContainer, ResultHeader, Results, Subtitle } from './style.js';
+
+const Result = ({ queryResults }) => {
   return (
     <ResContainer>
       <Subtitle>문제 뽑기 결과</Subtitle>
       <Results>
-        <div className="resultHeader">
+        <ResultHeader>
           <span>난이도 </span>
           <span>번호 </span>
           <span>카테고리</span>
-        </div>
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
-        <ResEntity num="1001" tier="Bronze 1" cate="구현" />
+        </ResultHeader>
+        <>
+          {queryResults ? (
+            queryResults.map((problem) => (
+              <ResEntity
+                num={problem.num}
+                tier={problem.level}
+                cate={problem.category.toString()}
+              />
+            ))
+          ) : (
+            <></>
+          )}
+        </>
       </Results>
     </ResContainer>
   );
