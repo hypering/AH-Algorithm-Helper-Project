136ec4396afc6244a498b46d05e88768569bd550
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-28 15:49:32 +0900

    [feat] 남은 시간 계산해서 보여주는 로직 작성

--- fe/src/components/MainContainer/Main/Calendar/Item/index.jsx
+++ fe/src/components/MainContainer/Main/Calendar/Item/index.jsx
@@ -32,6 +32,24 @@ const getDate = (date) => {
   return rst;
 };
 
+const diffTime = (date) => {
+  const contestDate = new Date(date).getTime();
+  const now = new Date().getTime();
+  console.log(new Date(date));
+  console.log(new Date());
+  let diff = Math.abs(contestDate - now);
+  const day = Math.round(diff / 86400000);
+  if (day >= 3) {
+    return `Before start ${day} days`;
+  }
+  const h = Math.floor(diff / 3600000);
+  diff %= 3600000;
+  const m = Math.floor(diff / 60000);
+  diff %= 60000;
+  const s = Math.floor(diff / 1000);
+  return `Before start ${h}:${m}:${s}`;
+};
+
 const Item = ({ id, value }) => {
   return (
     <ItemContainer>
@@ -47,7 +65,7 @@ const Item = ({ id, value }) => {
                 <ContestWrap onClick={onClick}>
                   <ContestTitle>{ele.title}</ContestTitle>
                   <ContestDate>{getDate(ele.startDate)}</ContestDate>
-                  <ContestTimer>{getDate(ele.startDate)}</ContestTimer>
+                  <ContestTimer>{diffTime(ele.startDate)}</ContestTimer>
                 </ContestWrap>
               );
             }
