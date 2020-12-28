1079fceb332482b305ccba3f5bb1ef441ee22e5f
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-28 15:29:40 +0900

    버튼 폴더 이동

---
+++ fe/src/components/MainContainer/Main/Buttons/Execute.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { Execute } from './style';
+
+const BtnExecute = ({ onClick }) => {
+  return (
+    <Execute icon={faCheckCircle} size="2x" color="#707070" onClick={onClick} />
+  );
+};
+export default BtnExecute;
