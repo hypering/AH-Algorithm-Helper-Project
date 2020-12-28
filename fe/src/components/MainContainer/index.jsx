6d03439ffba8cb75229bf75bb37d27f2e66a5e5c
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-28 15:26:06 +0900

    [feat]Browser Router 추가

--- fe/src/components/MainContainer/index.jsx
+++ fe/src/components/MainContainer/index.jsx
@@ -2,16 +2,18 @@ import React, { useState } from 'react';
 import SideNav from './SideNav';
 import Main from './Main';
 import { Container } from './style';
-
+import { BrowserRouter } from 'react-router-dom';
 // 여기서 변수를 한개 설정해서 SideNav로 넘겨준다
 // 넘겨받은 siveNav에서 onClick 이벤트 적용
 const MainContainer = () => {
   const [value, setValue] = useState(0);
   return (
-    <Container>
-      <SideNav setValue={setValue} />
-      <Main value={value} />
-    </Container>
+    <BrowserRouter>
+      <Container>
+        <SideNav setValue={setValue} />
+        <Main value={value} />
+      </Container>
+    </BrowserRouter>
   );
 };
 
