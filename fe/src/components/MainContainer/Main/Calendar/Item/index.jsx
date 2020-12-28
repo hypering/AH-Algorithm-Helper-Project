5a7fee1b9da9980b3a55e47b82a9e19c26670c5a
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-28 13:59:48 +0900

    [reafctor] 시간을 깔끔하게 변경

--- fe/src/components/MainContainer/Main/Calendar/Item/index.jsx
+++ fe/src/components/MainContainer/Main/Calendar/Item/index.jsx
@@ -7,6 +7,29 @@ import {
   UndeLine,
 } from './style';
 
+const pad = (number, length) => {
+  var str = '' + number;
+  while (str.length < length) {
+    str = '0' + str;
+  }
+  return str;
+};
+
+const getDate = (date) => {
+  const currentDate = new Date(date);
+  const rst =
+    pad(currentDate.getFullYear(), 2) +
+    '-' +
+    pad(currentDate.getMonth() + 1, 2) +
+    '-' +
+    pad(currentDate.getDate(), 2) +
+    ' ' +
+    pad(currentDate.getHours(), 2) +
+    ':' +
+    pad(currentDate.getMinutes(), 2);
+  return rst;
+};
+
 const Item = ({ id, value }) => {
   return (
     <ItemContainer>
@@ -19,7 +42,7 @@ const Item = ({ id, value }) => {
                 <ContestWrap>
                   <div>{ele.title}</div>
                   <UndeLine />
-                  <div>{ele.startDate}</div>
+                  <div>{getDate(ele.startDate)}</div>
                 </ContestWrap>
               );
             }
