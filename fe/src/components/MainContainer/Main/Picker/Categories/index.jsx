da8bf94e3494d6f6a96fc11c78e88abf4fd50d54
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-31 16:55:21 +0900

    [fix]key값 적용 및 중복 제거

--- fe/src/components/MainContainer/Main/Picker/Categories/index.jsx
+++ fe/src/components/MainContainer/Main/Picker/Categories/index.jsx
@@ -37,7 +37,6 @@ const CategoryContainer = ({ selectedCate, setSelectedCate }) => {
     '최대 유량',
     '우선순위 큐',
     '최소 스패닝 트리',
-    '위상 정렬',
     '최소 공통 조상',
     '에라토스테네스의 체',
     '플로이드-와샬',
@@ -53,6 +52,7 @@ const CategoryContainer = ({ selectedCate, setSelectedCate }) => {
       <Categories>
         {categories.map((element) => (
           <Category
+            key={element}
             name={element}
             selectedCate={selectedCate}
             setSelctedCate={setSelectedCate}
