d717e3e240dfaa87e02fcaab3aa88f753f5c4914
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2020-12-31 15:46:52 +0900

    [feat] 특별한 사람만 대회 등록을 할 수 있도록 권한 수정 및 대회 삭제 API 작성

--- be/routes/contest.js
+++ be/routes/contest.js
@@ -4,22 +4,24 @@ const router = express.Router();
 
 router.get('/', async (req, res) => {
   const now = new Date();
-  // const ip_addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  now.setHours(now.getHours() + 9);
+  now.setHours(now.getHours());
   const contestDates = await contestModel
     .find({
-      $and: [
-        {
-          startDate: { $gte: now },
-        },
-      ],
+      startDate: { $gte: now },
     })
     .sort({ startDate: 0 });
+  console.log(contestDates);
   res.json(contestDates);
 });
 
 router.post('/add', async (req, res) => {
   const { name, title, startDate, url } = req.body;
+  const ip_addr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+
+  if (ip_addr !== '::1') {
+    return res.status(201).json(false);
+  }
+
   await contestModel.create({
     name,
     title,
@@ -28,6 +30,15 @@ router.post('/add', async (req, res) => {
   });
   console.log(startDate);
   res.status(200).json(true);
+});
+
+router.post('/delete', async (req, res) => {
+  const { contest_id } = req.body;
+  const response = await contestModel.deleteOne({
+    _id: contest_id,
+  });
+  if (response.deletedCount) res.status(200).json(true);
+  else res.status(200).json(false);
 });
 
 module.exports = router;
