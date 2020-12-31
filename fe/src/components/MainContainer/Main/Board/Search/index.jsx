ed3cb0c5d1d1a1cf515c95e9d4b8b23b5383400c
pjh612 <62292492+pjh612@users.noreply.github.com>
2020-12-31 16:53:46 +0900

    [feat]검색 결과 추가 및 스타일링

--- fe/src/components/MainContainer/Main/Board/Search/index.jsx
+++ fe/src/components/MainContainer/Main/Board/Search/index.jsx
@@ -1,4 +1,61 @@
-const Search = (results) => {
-  return results;
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import Post from '../Board/Post';
+import queryString from 'query-string';
+import DetailView from '../DetailView';
+import { SubContainer as Container } from '../style';
+import { Container as SubContainer } from './style';
+const Search = ({
+  posts,
+  setBoards,
+  setValue,
+  value,
+  selectedBoard,
+  setSelectedBoard,
+  searchType,
+}) => {
+  const [searchResult, setSearchResult] = useState(null);
+  const location = useLocation();
+  const queryObj = queryString.parse(location.search);
+
+  useEffect(() => {
+    setValue(queryObj.value);
+    if (posts) {
+      if (searchType === 'author')
+        setSearchResult(posts.filter((post) => post.author === value));
+      else {
+        setSearchResult(
+          posts.filter((post) => {
+            return post.tags.includes(value);
+          })
+        );
+      }
+    }
+  }, [posts]);
+
+  return (
+    <>
+      {searchResult && searchResult.length >= 1 ? (
+        <Container>
+          <SubContainer>
+            {searchResult.map((result) => (
+              <Post
+                posts={posts}
+                setBoards={setBoards}
+                post={result}
+                id={result._id}
+                key={result._id}
+                setSelectedBoard={setSelectedBoard}
+              ></Post>
+            ))}
+          </SubContainer>
+          <DetailView post={selectedBoard} />
+        </Container>
+      ) : (
+        <div>검색 결과 없음</div>
+      )}
+    </>
+  );
 };
 export default Search;
