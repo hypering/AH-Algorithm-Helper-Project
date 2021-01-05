9509794dc42dd0db80daf75b1af9f52e861be915
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-05 14:44:52 +0900

    [refactor] 실시간으로 댓글이 업데이트되도록 수정
    댓글이 등록되었을 경우, board를 업데이트하고 넣어준 뒤, 리렌더링 시킨다.

--- fe/src/components/MainContainer/Main/Board/DetailView/index.jsx
+++ fe/src/components/MainContainer/Main/Board/DetailView/index.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import { CommentStateContext } from '../../Board';
 import { CommentDispatchContext } from '../../Board';
+import { getDate } from '../../../../../lib/getTimer';
 import {
   Container,
   ImgIcon,
@@ -13,7 +14,7 @@ import {
   CommentText,
 } from './style';
 
-const DetailView = ({ post, setUpdateBoard }) => {
+const DetailView = ({ posts, post, setBoards }) => {
   if (!post) return <EmptyText>선택된 글이 없습니다.</EmptyText>;
   const value = useContext(CommentStateContext);
   const dispatch = useContext(CommentDispatchContext);
@@ -30,12 +31,13 @@ const DetailView = ({ post, setUpdateBoard }) => {
       alert('댓글을 입력하세요!');
       return;
     }
-
+    const nowDate = getDate(new Date());
     const response = await fetch('http://localhost:4000/board/comment/write', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         boardId: post._id,
+        createAt: nowDate,
         context: value,
       }),
     });
@@ -49,7 +51,13 @@ const DetailView = ({ post, setUpdateBoard }) => {
       alert('댓글 등록에 실패하였습니다.');
       return;
     }
-    setUpdateBoard({});
+    post.comment.push({
+      createAt: nowDate,
+      context: value,
+    });
+
+    const updatedPosts = [...posts];
+    setBoards(updatedPosts);
   };
 
   return (
