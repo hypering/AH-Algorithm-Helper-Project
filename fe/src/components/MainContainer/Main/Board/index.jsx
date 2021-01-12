df9e025d0a2d3539dcd95edf410cc2474a106569
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-12 17:03:03 +0900

    [refactor] Switch Router Dom을 추가하여 해당 url이 아닐 경우 404 페이지를 보이도록 구현

--- fe/src/components/MainContainer/Main/Board/index.jsx
+++ fe/src/components/MainContainer/Main/Board/index.jsx
@@ -41,43 +41,46 @@ const FreeBoard = ({ curIp }) => {
     <CommentDispatchContext.Provider value={dispatch}>
       <CommentStateContext.Provider value={state}>
         <Container>
-          <Route path="/board/write" component={Write} />
-          <Route exact path="/board">
-            <Header
-              value={value}
-              setValue={setValue}
-              url={url}
-              setUrl={setUrl}
-              searchType={searchType}
-              setSearchType={setSearchType}
-            />
-            <SubContainer>
-              <Board
-                posts={boards}
-                setBoards={setBoards}
-                setSelectedBoard={setSelectedBoard}
-                curIp={curIp}
+          <Switch>
+            <Route exact path="/board/write" component={Write} />
+            <Route exact path="/board">
+              <Header
+                value={value}
+                setValue={setValue}
+                url={url}
+                setUrl={setUrl}
+                searchType={searchType}
+                setSearchType={setSearchType}
               />
-              <DetailView
-                posts={boards}
-                post={selectedBoard}
-                setBoards={setBoards}
-                setSelectedBoard={setSelectedBoard}
-              />
-            </SubContainer>
-          </Route>
+              <SubContainer>
+                <Board
+                  posts={boards}
+                  setBoards={setBoards}
+                  setSelectedBoard={setSelectedBoard}
+                  curIp={curIp}
+                />
+                <DetailView
+                  posts={boards}
+                  post={selectedBoard}
+                  setBoards={setBoards}
+                  setSelectedBoard={setSelectedBoard}
+                />
+              </SubContainer>
+            </Route>
 
-          <Route path="/board/search">
-            <Search
-              value={value}
-              posts={boards}
-              setValue={setValue}
-              setBoards={setBoards}
-              selectedBoard={selectedBoard}
-              setSelectedBoard={setSelectedBoard}
-              searchType={searchType}
-            ></Search>
-          </Route>
+            <Route path="/board/search">
+              <Search
+                value={value}
+                posts={boards}
+                setValue={setValue}
+                setBoards={setBoards}
+                selectedBoard={selectedBoard}
+                setSelectedBoard={setSelectedBoard}
+                searchType={searchType}
+              ></Search>
+            </Route>
+            <Route render={() => <div>404 NOT FOUND</div>} />
+          </Switch>
         </Container>
       </CommentStateContext.Provider>
     </CommentDispatchContext.Provider>
