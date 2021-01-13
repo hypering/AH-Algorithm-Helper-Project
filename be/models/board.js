fe68c8fab662f411f675ba53888dcb505399d583
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-13 17:09:07 +0900

    [feat]addComment 함수 추가

--- be/models/board.js
+++ be/models/board.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 
 const { Schema } = mongoose;
-
+const commentSchema = new mongoose.Schema({
+  writerId: String,
+  createAt: { type: Date, default: Date.now() },
+  context: String,
+});
 const boardSchema = new Schema(
   {
     author: String,
@@ -10,17 +14,24 @@ const boardSchema = new Schema(
     tags: [String],
     content: String,
     heart: [String],
-    comment: [Object],
+    comment: [commentSchema],
     clicked: [String],
   },
   { versionKey: false },
 );
 
 const boardModel = mongoose.model('board', boardSchema);
+const addComment = async function (boardId, commentBody) {
+  const queryPost = await boardModel.findOne({ _id: boardId });
+  const newComment = await queryPost.comment.push(commentBody);
 
+  await queryPost.save();
+
+  return queryPost.comment[queryPost.comment.length - 1]._id;
+};
 boardSchema.statics.create = function (body) {
   const newArticle = new this(body);
   return newArticle.save();
 };
 
-module.exports = boardModel;
+module.exports = { boardModel, addComment };
