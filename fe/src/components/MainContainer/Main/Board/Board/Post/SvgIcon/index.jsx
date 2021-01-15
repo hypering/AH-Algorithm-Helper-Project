85f4d1c67c3bc5703404e0b522ffd50fc73257e6
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-15 17:37:53 +0900

    [feat]유저가 좋아요한 게시글 표시

--- fe/src/components/MainContainer/Main/Board/Board/Post/SvgIcon/index.jsx
+++ fe/src/components/MainContainer/Main/Board/Board/Post/SvgIcon/index.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Container, Icon } from './style';
 
-const SvgIcon = ({ path, text, color, className, onClick }) => {
+const SvgIcon = ({ bgColor, path, text, color, className, onClick }) => {
   return (
-    <Container color={color} onClick={onClick}>
-      <Icon color={color}>
+    <Container bgColor={bgColor} color={color} onClick={onClick}>
+      <Icon bgColor={bgColor} color={color}>
         <g>
           <path d={path} />
         </g>
