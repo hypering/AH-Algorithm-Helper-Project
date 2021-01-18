7837ba5d3081c167700b2b8c9ff0ed926c967339
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-18 22:30:38 +0900

    [style]페이지 전체 대규모 스타일링 변경

--- fe/src/components/MainContainer/Main/Board/Board/style.js
+++ fe/src/components/MainContainer/Main/Board/Board/style.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 export const Container = styled.div`
   display: flex;
-  width: 45%;
+  width: 400px;
   flex-direction: column;
   padding: 15px;
   box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.15);
