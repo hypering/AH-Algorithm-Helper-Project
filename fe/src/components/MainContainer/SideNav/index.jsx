1c73ce35134526ebdad0c8ba0a49c8316b0f0a5c
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-21 14:58:12 +0900

    [refactor]NavLink 사용으로 코드 간결화,  기존 로그인 정보 로드 코드 삭제  loadLoginInfo() 추가

--- fe/src/components/MainContainer/SideNav/index.jsx
+++ fe/src/components/MainContainer/SideNav/index.jsx
@@ -1,61 +1,20 @@
-import React, { useRef, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { SideContainer, Menus, Menu } from './style';
 
-const PICKER_NUMBER = 0;
-const CALENDAR_NUMBER = 1;
-const BOARD_NUMBER = 2;
-
-const ChangeColor = (selected, ...notSelected) => {
-  selected.current.style.color = 'white';
-  selected.current.style.backgroundColor = '#707070';
-  notSelected[0].current.style.color = '#707070';
-  notSelected[0].current.style.backgroundColor = 'white';
-  notSelected[1].current.style.color = '#707070';
-  notSelected[1].current.style.backgroundColor = 'white';
-};
-
-const setSelector = (number, pickerBtn, calendarBtn, boardBtn) => {
-  if (number == PICKER_NUMBER) {
-    ChangeColor(pickerBtn, calendarBtn, boardBtn);
-  } else if (number == CALENDAR_NUMBER) {
-    ChangeColor(calendarBtn, pickerBtn, boardBtn);
-  } else if (number == BOARD_NUMBER) {
-    ChangeColor(boardBtn, pickerBtn, calendarBtn);
-  }
-};
-
-const SideNav = ({ value, setValue }) => {
-  const pickerBtn = useRef();
-  const calendarBtn = useRef();
-  const boardBtn = useRef();
-
-  const pickerOnclick = () => setValue(PICKER_NUMBER);
-  const calendarOnclick = () => setValue(CALENDAR_NUMBER);
-  const BoardOnclick = () => setValue(BOARD_NUMBER);
-
-  useEffect(() => {
-    setSelector(value, pickerBtn, calendarBtn, boardBtn);
-  }, [value]);
-
+const SideNav = () => {
   return (
     <SideContainer>
       <Menus>
-        <Link to="/picker">
-          <Menu onClick={pickerOnclick} ref={pickerBtn}>
-            문제 뽑기
-          </Menu>
-        </Link>{' '}
-        <Link to="/calendar">
-          <Menu onClick={calendarOnclick} ref={calendarBtn}>
-            대회 일정
-          </Menu>
-        </Link>{' '}
-        <Link to="/board">
-          <Menu onClick={BoardOnclick} ref={boardBtn}>
-            커뮤니티
-          </Menu>
-        </Link>
+        <NavLink to="/picker">
+          <Menu>문제 뽑기</Menu>
+        </NavLink>
+        <NavLink to="/calendar">
+          <Menu>대회 일정</Menu>
+        </NavLink>
+        <NavLink to="/board">
+          <Menu>커뮤니티</Menu>
+        </NavLink>
       </Menus>
     </SideContainer>
   );
--- fe/src/components/MainContainer/index.jsx
+++ fe/src/components/MainContainer/index.jsx
@@ -4,29 +4,12 @@ import SideNav from './SideNav';
 import Main from './Main';
 import { Container } from './style';
 import { UserDispatch } from '../../App';
+import loadLoginInfo from '../../hooks/loadLoginInfo';
 const MainContainer = ({ curIp }) => {
   const dispatch = useContext(UserDispatch);
-  const location = useLocation().pathname.substr(1);
   const [value, setValue] = useState(0);
   useEffect(() => {
-    if (location.includes('picker')) setValue(0);
-    if (location.includes('calendar')) setValue(1);
-    if (location.includes('board')) setValue(2);
-  }, [location]);
-  useEffect(() => {
-    fetch(`${process.env.BASE_URL}/user`, {
-      method: 'post',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      credentials: 'include',
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        dispatch({ type: 'SET_IS_LOGINED', payload: json });
-      });
+    loadLoginInfo(dispatch);
   }, []);
   return (
     <Container>
