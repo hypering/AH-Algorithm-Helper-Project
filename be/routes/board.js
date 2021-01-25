45c6f42ed2552964a8427e4a7560399c5ddccf9e
DESKTOP-J5O26G9\admin <glesmallow@gmail.com>
2021-01-25 16:45:33 +0900

    [refactor] route에서 DB 로직을 Service 폴더에 따로 분리해서 저장

--- be/routes/board.js
+++ be/routes/board.js
@@ -1,58 +1,32 @@
 const express = require('express');
 const { upload } = require('../lib/imageUpload');
-const refinePostDatas = require('../lib/refinePostDatas');
-const { boardModel, addComment } = require('../models');
-const { userModel } = require('../models');
+const BoardService = require('../services/board');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
   const { startIdx } = req.query;
-  const boardDatas = await boardModel.find().sort({ _id: -1 }).skip(Number(startIdx)).limit(5);
-  const refinedDatas = await refinePostDatas(boardDatas);
-  res.json(refinedDatas);
+  const selectResult = await BoardService.selectBoard({ startIdx });
+
+  res.json(selectResult);
 });
 
 router.post('/search', async (req, res) => {
   const { type, value } = req.body;
 
-  if (type === 'author') {
-    const queryUser = await userModel.findOne({ userId: value });
+  const searchResult = await BoardService.searchBoard({ type, value });
 
-    const searchResults = await boardModel.find({ author: queryUser._id }).sort({ _id: -1 });
-    const refinedDatas = await refinePostDatas(searchResults);
-
-    console.log(searchResults);
-    res.status(200).json(refinedDatas);
-  } else if (type === 'tag') {
-    console.log(type);
-    const searchResults = await boardModel.find().sort({ _id: -1 });
-    console.log(searchResults);
-    const filterDatas = searchResults.filter((post) => {
-      return post.tags.includes(value);
-    });
-    const refinedDatas = await refinePostDatas(filterDatas);
-    res.status(200).json(refinedDatas);
-  } else res.status(204).json(false);
+  if (type === 'author' || type === 'tag') return res.status(200).json(searchResult);
+  res.status(204).json(false);
 });
+
 router.post('/viewup', async (req, res) => {
   const contentId = req.body.contentId;
   const curIp = req.body.curIp;
 
-  const queryContent = await boardModel.findOne({ _id: contentId });
+  const viewUpResult = await BoardService.addBoardView({ contentId, curIp });
 
-  const isExist = queryContent.clicked.filter((element) => {
-    return element === curIp;
-  });
-  if (isExist.length == 0) {
-    queryContent.clicked.push(curIp);
-    queryContent.save();
-    res.json({ status: 200, clicked: queryContent.clicked.length });
-    console.log('새로운 ip');
-  } else {
-    console.log('이미존재하는  ip');
-    res.json({ status: 404 });
-  }
+  res.json(viewUpResult);
 });
 
 router.post('/imageupload', upload.single('img'), function (req, res) {
@@ -61,23 +35,12 @@ router.post('/imageupload', upload.single('img'), function (req, res) {
 });
 
 router.post('/write', async (req, res) => {
+  if (!req.session.user) res.status(401).json(false);
   const { content, tags, imgName } = req.body;
+  const user = req.session.user;
 
-  const hash = tags.split(',').filter((e) => {
-    return e.length >= 1;
-  });
-  const author = req.session.user._id;
-  const newPost = await boardModel.create({
-    author,
-    img_url: imgName,
-    content: content,
-    tags: hash,
-  });
-  console.log('newPost', newPost);
-  const queryUser = await userModel.findOne({ _id: author });
-  console.log('queryUser', queryUser);
-  await queryUser.posts.push(newPost._id);
-  queryUser.save();
+  await BoardService.writeBoard({ content, tags, imgName, user });
+
   res.statusCode = 302;
   if (process.env.ENV === 'development') {
     res.setHeader('Location', 'http://127.0.0.1:3000/board');
@@ -88,69 +51,50 @@ router.post('/write', async (req, res) => {
 });
 
 router.post('/heartup', async (req, res) => {
+  if (!req.session.user) res.status(401).json(false);
   const contentId = req.body.contentId;
-  const queryContent = await boardModel.findOne({ _id: contentId });
-  const isExist = queryContent.heart.filter((element) => {
-    return element === req.session.user._id;
-  });
-  if (isExist.length == 0) {
-    queryContent.heart.push(req.session.user._id);
-    queryContent.save();
-    res.json({ status: 200, clicked: queryContent.heart.length });
-    console.log('새로운 ip');
-  } else {
-    console.log('이미존재하는  ip');
-    res.json({ status: 404 });
-  }
+  const user = req.session.user;
+
+  const heartResult = await BoardService.addBoardHeart({ contentId, user });
+
+  res.json(heartResult);
 });
 
 router.post('/comment/write', async (req, res) => {
   const { boardId, createAt, context } = req.body;
   const writer = req.session.user._id;
-  const newCommentId = await addComment(boardId, {
-    writerId: writer,
+
+  const writeCommentResult = await BoardService.writeBoardComment({
+    writer,
+    boardId,
     createAt,
     context,
   });
 
-  res.status(200).json({ newCommentId });
+  res.status(200).json({ writeCommentResult });
 });
 
 router.post('/comment/delete', async (req, res) => {
-  const { boardId, key } = req.body;
-  const queryContent = await boardModel.findOne({ _id: boardId });
-  console.log(key);
-  const removedComment = queryContent.comment.find((element) => {
-    console.log(typeof element._id, typeof key);
-    return String(element._id) === key;
-  });
+  if (!req.session.user) res.status(401).json(false);
 
-  if (removedComment !== undefined) {
-    await boardModel.findOneAndUpdate(
-      {
-        _id: boardId,
-      },
-      {
-        $pull: { comment: removedComment },
-      },
-      { new: true },
-    );
-    res.status(200).json(true);
-    return;
-  }
-  res.status(201).json(true);
+  const { boardId, key } = req.body;
+
+  const deleteCommentResult = await BoardService.deleteBoardComment({ boardId, key });
+
+  if (deleteCommentResult) res.status(200).json(true);
+  else res.status(201).json(false);
 });
 
 router.post('/delete', async (req, res) => {
   const { boardId } = req.body;
-  if (!req.session.user) res.status(401).json(false);
-  const queryContent = await boardModel.deleteOne({ _id: boardId });
-  const queryUser = await userModel.findOne({ _id: req.session.user._id });
-  const idx = queryUser.posts.findIndex((e) => e === boardId);
-  queryUser.posts.splice(idx, 1);
-  await queryUser.save();
-  if (queryContent !== undefined) {
+  const user = req.session.user;
+  if (!user) res.status(401).json(false);
+
+  const deleteResult = await BoardService.deleteBoard({ boardId, user });
+
+  if (deleteResult) {
     res.status(200).json(true);
   } else res.status(201).json(false);
 });
+
 module.exports = router;
