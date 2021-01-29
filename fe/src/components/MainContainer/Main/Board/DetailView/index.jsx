fcb4d79d7293df531bf89b6f2d4854fac848378b
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-29 18:32:54 +0900

    [refactor]ExitButton, PostDeleteButton 컴포넌트화

--- fe/src/components/MainContainer/Main/Board/DetailView/index.jsx
+++ fe/src/components/MainContainer/Main/Board/DetailView/index.jsx
@@ -21,6 +21,7 @@ import {
 import { IsLoginedState } from 'App';
 import { Link } from 'react-router-dom';
 import API from 'lib/api';
+import PostDeleteButton from '../../../Buttons/PostDeleteButton';
 
 const DetailView = ({
   posts,
@@ -103,20 +104,7 @@ const DetailView = ({
   return (
     <Container isModal={isModal}>
       {post.authorKey === isLogined.userKey && (
-        <svg
-          aria-label="닫기"
-          fill="#00000"
-          height="24"
-          viewBox="0 0 48 48"
-          width="24"
-          onClick={onDeleteClick}
-        >
-          <path
-            clipRule="evenodd"
-            d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z"
-            fillRule="evenodd"
-          ></path>
-        </svg>
+        <PostDeleteButton onClick={onDeleteClick} />
       )}
       <Link to={`/account/${post.author}`}>
         <UserInfoContainer>
--- fe/src/components/MainContainer/Main/Board/PostModal/index.jsx
+++ fe/src/components/MainContainer/Main/Board/PostModal/index.jsx
@@ -1,30 +1,15 @@
 import React from 'react';
-
+import ExitButton from '../../../Buttons/ExitButton';
 import Post from '../Board/Post';
 import DetailView from '../DetailView';
-import { ButtonExit, Container, ModalContainer } from './style';
-
+import { Container, ModalContainer } from './style';
 const PostModal = ({ posts, post, setModalPost, setPosts }) => {
   const onExitClick = () => {
     setModalPost(null);
   };
   return (
     <Container>
-      <ButtonExit onClick={onExitClick}>
-        <svg
-          aria-label="닫기"
-          fill="#ffffff"
-          height="24"
-          viewBox="0 0 48 48"
-          width="24"
-        >
-          <path
-            clipRule="evenodd"
-            d="M41.8 9.8L27.5 24l14.2 14.2c.6.6.6 1.5 0 2.1l-1.4 1.4c-.6.6-1.5.6-2.1 0L24 27.5 9.8 41.8c-.6.6-1.5.6-2.1 0l-1.4-1.4c-.6-.6-.6-1.5 0-2.1L20.5 24 6.2 9.8c-.6-.6-.6-1.5 0-2.1l1.4-1.4c.6-.6 1.5-.6 2.1 0L24 20.5 38.3 6.2c.6-.6 1.5-.6 2.1 0l1.4 1.4c.6.6.6 1.6 0 2.2z"
-            fillRule="evenodd"
-          ></path>
-        </svg>
-      </ButtonExit>
+      <ExitButton onClick={onExitClick} />
       <ModalContainer>
         <Post setBoards={setPosts} posts={posts} post={post} isModal="true" />
         <DetailView
