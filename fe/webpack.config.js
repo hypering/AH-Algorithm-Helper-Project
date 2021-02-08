8fe6ef2164c2971156a339049d6257ae3afd29d3
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-02-08 14:33:57 +0900

    [fix]웹팩 이미지 경로 오류 수정

--- fe/webpack.config.js
+++ fe/webpack.config.js
@@ -53,7 +53,7 @@ module.exports = {
         loader: 'file-loader',
         options: {
           name: '[name].[ext]',
-          publicPath: '/',
+          publicPath: '/Images',
           outputPath: '/Images',
         },
       },
